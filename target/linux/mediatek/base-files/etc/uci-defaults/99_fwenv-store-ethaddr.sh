1e14a6627d4ea9a89cce1d8a1dbb3c4a7f5abba9
dante <dante@seckv.com>
2026-07-10 16:10:48 +0800

    [mediatek] 合并 openwrt-mtk 自定义配置与 PPE 动态表项内核补丁

--- target/linux/mediatek/base-files/etc/uci-defaults/99_fwenv-store-ethaddr.sh
+++ target/linux/mediatek/base-files/etc/uci-defaults/99_fwenv-store-ethaddr.sh
@@ -11,14 +11,27 @@ unielec,u7623-02)
 	;;
 bananapi,bpi-r3|\
 bananapi,bpi-r3-mini|\
-bananapi,bpi-r4|\
-bananapi,bpi-r4-2g5|\
-bananapi,bpi-r4-lite|\
-bananapi,bpi-r4-poe)
+bananapi,bpi-r4-lite)
 	[ -z "$(fw_printenv -n ethaddr 2>/dev/null)" ] &&
 		fw_setenv ethaddr "$(cat /sys/class/net/eth0/address)"
 	[ -z "$(fw_printenv -n eth1addr 2>/dev/null)" ] &&
 		fw_setenv eth1addr "$(macaddr_add $(cat /sys/class/net/eth0/address) 1)"
+	;;
+bananapi,bpi-r4|\
+bananapi,bpi-r4-2g5|\
+bananapi,bpi-r4-poe)
+	local need_reboot=0
+	[ -z "$(fw_printenv -n ethaddr 2>/dev/null)" ] &&
+		fw_setenv ethaddr "$(cat /sys/class/net/eth0/address)"
+	if [ -z "$(fw_printenv -n eth1addr 2>/dev/null)" ]; then
+		fw_setenv eth1addr "$(macaddr_add $(cat /sys/class/net/eth0/address) 1)"
+		need_reboot=1
+	fi
+	if [ -z "$(fw_printenv -n eth2addr 2>/dev/null)" ]; then
+		fw_setenv eth2addr "$(macaddr_add $(cat /sys/class/net/eth0/address) 5)"
+		need_reboot=1
+	fi
+	[ "$need_reboot" -eq 1 ] && (sleep 3; reboot) &
 	;;
 esac
 
